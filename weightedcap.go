93233431cdd1b173f38cdd7444ab9ce4ebe2c6e7
Romain Menke <romainmenke@gmail.com>
2018-01-22 19:01:30 +0100

    weighted capacity

---
+++ weightedcap.go
@@ -0,0 +1,74 @@
+package weightedcap
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type weightedCap struct {
+	capacity    int64
+	maxCapacity int64
+
+	signalMu sync.Mutex
+	signal   chan struct{}
+}
+
+func New(capacity int64) *weightedCap {
+	return &weightedCap{
+		maxCapacity: capacity,
+		capacity:    capacity,
+		signal:      make(chan struct{}, 1),
+	}
+}
+
+func (w *weightedCap) Push(ctx context.Context, n int64) error {
+	if n > w.maxCapacity {
+		return &ExceedingCapacityErr{n, w.maxCapacity}
+	}
+	if atomic.LoadInt64(&w.capacity) >= n {
+		w.push(ctx, n)
+		return nil
+	}
+
+	w.signalMu.Lock()
+	defer w.signalMu.Unlock()
+
+	for atomic.LoadInt64(&w.capacity) < n {
+		err := w.waitForSignalLocked(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	w.push(ctx, n)
+	return nil
+}
+
+func (w *weightedCap) waitForSignalLocked(ctx context.Context) error {
+	select {
+	case <-w.signal:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (w *weightedCap) push(ctx context.Context, n int64) {
+	atomic.AddInt64(&w.capacity, -n)
+}
+
+func (w *weightedCap) Pop(n int64) {
+	atomic.AddInt64(&w.capacity, n)
+	w.signal <- struct{}{}
+}
+
+type ExceedingCapacityErr struct {
+	Capacity    int64
+	MaxCapacity int64
+}
+
+func (e *ExceedingCapacityErr) Error() string {
+	return fmt.Sprintf("weightedcap: requested capacity (%d) higher than the maximum capacity (%d)", e.Capacity, e.MaxCapacity)
+}
---
+++ weightedcap_test.go
@@ -0,0 +1,92 @@
+package weightedcap_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/romainmenke/weightedcap"
+)
+
+func TestPushPop_NoLoad(t *testing.T) {
+	cap := weightedcap.New(3)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*5)
+	defer cancel()
+
+	// no load test at max capacity
+	err := cap.Push(ctx, 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cap.Pop(3)
+}
+
+func TestPushPop_Load(t *testing.T) {
+	cap := weightedcap.New(3)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
+	defer cancel()
+
+	// add some load
+	{
+		err := cap.Push(ctx, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// release load after some time on other goroutine
+	go func() {
+		time.Sleep(time.Millisecond * 5)
+		cap.Pop(2)
+	}()
+
+	// attempt to add more load
+	{
+		err := cap.Push(ctx, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer cap.Pop(2)
+	}
+}
+
+func TestPushPop_Timeout(t *testing.T) {
+	cap := weightedcap.New(3)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*1)
+	defer cancel()
+
+	time.Sleep(time.Millisecond * 2)
+
+	// plenty of capacity, so will not block and Push will succeed.
+	{
+		err := cap.Push(ctx, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// not enough capacity and timeout happened, must return error.
+	{
+		err := cap.Push(ctx, 2)
+		if err != ctx.Err() {
+			t.Fatal(fmt.Sprintf("expected ctx err, got : %v", err))
+		}
+	}
+}
+
+func TestPushPop_NegativeCap(t *testing.T) {
+	cap := weightedcap.New(3)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*1)
+	defer cancel()
+
+	expectedErr := &weightedcap.ExceedingCapacityErr{5, 3}
+	err := cap.Push(ctx, 5)
+	if err.Error() != expectedErr.Error() {
+		t.Fatal(fmt.Sprintf("expected : %v, got : %v", expectedErr, err))
+	}
+}
